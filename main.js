8fec2e447ad7fc5b29c33491bbcf1d2c1154705a
MarianoBordon8 <bordon.marianooscar@gmail.com>
2023-08-01 16:59:10 -0300

    Segunda PreEntrega

--- main.js
+++ main.js
@@ -2,60 +2,60 @@
 function productosDisponibles(productos){
     let cantidadProductos = parseInt(prompt('Ingrese la cantidad de Productos que desea agregar:'));
     for (let i = 3; i < (cantidadProductos + 3); i++) {
-        let id = i + 1
+        let id = i + 1;
         let nombre = prompt("Ingrese el nombre del producto " + (i + 1) + ':');
         while (nombre === null || nombre === ''){
-            console.log("Valor incorrecto, vuelva a ingresarlo")
-            nombre = prompt("Ingrese el nombre del producto " + (i + 1) + ':')
+            console.log("Valor incorrecto, vuelva a ingresarlo");
+            nombre = prompt("Ingrese el nombre del producto " + (i + 1) + ':');
         }
         let precio = parseInt(prompt("Ingrese el precio del producto " + (i + 1) + ':'));
         while (isNaN(precio)){
-            console.log("Valor incorrecto, vuelva a ingresarlo")
+            console.log("Valor incorrecto, vuelva a ingresarlo");
             precio = parseInt(prompt("Ingrese el precio del producto " + (i + 1) + ':'));
         }
         let nuevoProducto = {
             id: id,
             nombre: nombre,
             precio: precio
-        };
+        }
         productos.push(nuevoProducto);
     }
-    console.log(productos)
-    return productos
+    console.log(productos);
+    return productos;
 }
 //opcion2
 function mostrarStock(productos){
-    console.log("-------------------------------------------")
+    console.log("-------------------------------------------");
     for (const producto of productos){
-        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n")
+        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n");
     }
 }
 //opcion3
 function agregarAlCarrito(productos, carrito){
-    const idProducto = parseInt(prompt("Ingrese el ID del producto que desea agregar al carrito: "))
-    const encontrado = productos.includes(productos[idProducto - 1])
+    const idProducto = parseInt(prompt("Ingrese el ID del producto que desea agregar al carrito: "));
+    const encontrado = productos.includes(productos[idProducto - 1]);
     if (encontrado){
-        carrito.push(productos[idProducto - 1])
+        carrito.push(productos[idProducto - 1]);
     } else{
-        console.log("ID invalido")
+        console.log("ID invalido");
     }
-    carrito.sort(function(a, b){return a.id - b.id})
+    carrito.sort(function(a, b){return a.id - b.id});
 }
 //opcion4
 function mostrarCarrito(carrito){
-    console.log("-------------------------------------------")
+    console.log("-------------------------------------------");
     for (const producto of carrito){
-        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n")
+        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n");
     }
-    let total = 0
+    let total = 0;
     for (const i of carrito){
-        total += i.precio
+        total += i.precio;
     }
-    console.log("El total a pagar es de: $" + total)
+    console.log("El total a pagar es de: $" + total);
 }
 //opcion5
 function eliminarDeCarrito(carrito){
-    const idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar del carrito: "))
+    const idProducto = parseInt(prompt("Ingrese el ID del producto que desea eliminar del carrito: "));
     const encontrado = carrito.findIndex(producto => producto.id === idProducto);
     if (encontrado !== -1){
         carrito.splice(encontrado, 1);
@@ -65,25 +65,27 @@ function eliminarDeCarrito(carrito){
 }
 //opcion6
 function comprar(carrito, productos){
-    console.log("acabas de comprar: ")
+    console.log("acabas de comprar: ");
     for (const producto of carrito){
-        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n")
+        console.log("ID: " + producto.id + "- Nombre: " + producto.nombre + "- Precio: $" + producto.precio + "\n");
     }
-    let total = 0
+    let total = 0;
     for (const i of carrito){
-        total += i.precio
-        let idABorrar = i.id
-        let borrar = productos.findIndex(producto => producto.id === idABorrar);
-        productos.splice(borrar,1)
+        total += i.precio;
+        let idABorrar = i.id;
+        let borrarPro = productos.findIndex(producto1 => producto1.id === idABorrar);
+        productos.splice(borrarPro,1);
     }
-    console.log("por $" + total)
-    console.log("MUCHAS GRACIAS")
+    const cant = carrito.length;
+    carrito.splice(0,cant);
+    console.log("por $" + total);
+    console.log("MUCHAS GRACIAS");
 }
 //opcion7
 function vaciarCarrito(carrito){
     const resultado = confirm("¿Seguro de que deseas vaciar el carrito?");
     if (resultado){
-        carrito.splice(0,(carrito.length))
+        carrito.splice(0,(carrito.length));
     }
 }
 
@@ -92,7 +94,7 @@ function vaciarCarrito(carrito){
 
 
 let carrito = [];
-let productos = [{id: 1, nombre: 'pantalon', precio: 15000},{id: 2, nombre: 'campera', precio: 20000},{id: 3, nombre: 'medias', precio: 1500}];
+let productos = [{id: 1, nombre: "pantalon", precio: 15000},{id: 2, nombre: "campera", precio: 20000},{id: 3, nombre: "medias", precio: 1500}];
 let opcion = 0;
 
 while (opcion !== -1) {
@@ -113,29 +115,29 @@ while (opcion !== -1) {
             productos = productosDisponibles(productos);
             break;
         case 2:
-            mostrarStock(productos)
+            mostrarStock(productos);
             break;
         case 3:
-            agregarAlCarrito(productos, carrito)
+            agregarAlCarrito(productos, carrito);
             break;
         case 4:
-            mostrarCarrito(carrito)
+            mostrarCarrito(carrito);
             break;
         case 5:
-            eliminarDeCarrito(carrito)
+            eliminarDeCarrito(carrito);
             break;
         case 6:
-            comprar(carrito, productos)
+            comprar(carrito, productos);
             break;
         case 7:
-            vaciarCarrito(carrito)
+            vaciarCarrito(carrito);
             break;
         case 8:
-            console.log('Saliendo del menú...');
+            console.log("Gracias por participar");
             opcion = -1
             break; // Sale del bucle y finaliza el programa
         default:
-            console.log('Opción inválida, intenta de nuevo.');
+            console.log("Opción inválida, intenta de nuevo.");
             break;
     }
 }
